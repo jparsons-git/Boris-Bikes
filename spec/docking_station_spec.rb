7cb4899a3f7a08f59c2eb1df7298000552c02f6d
Eddie Arnold <eddiearnoldz@me.com>
2022-01-25 19:09:27 +0000

    Have tried to complete chapter 10 but can't get the tests to pass. This is because the two object ids for the Bike instances created are different. I think i need to make n instance varaiable but didn't have time to figure that out

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,14 @@ require_relative '../lib/docking_station'
 
 describe DockingStation do
   describe "#release_bike" do
-    # it "allows user to get bike from docking station" do
-    #   docking_station = DockingStation.new
-    #   expect(docking_station).to respond_to(:release_bike)
-    # end
-    it { is_expected.to respond_to(:release_bike) }
+    it "allows user to get bike from docking station" do
+      docking_station = DockingStation.new
+      expect(docking_station).to respond_to(:release_bike)
+    end
+    # it { is_expected.to respond_to(:release_bike) }
+    it "should create a new bike object" do
+      docking_station = DockingStation.new
+      expect(docking_station.release_bike).to equal (bike = Bike.new)
+    end
   end
 end
